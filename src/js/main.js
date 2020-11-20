f1858c654e3a592fa1480dbf069ed1dd09bb6b80
Jeff Chiu <jwchiu82@gmail.com>
2020-11-20 11:58:37 -0800

    add ability to display different currencies

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,17 @@ function clearFields() {
 
 function displayExchange(response) {
   $('.display-conversion').text(`The exchange rate of ${currency} ${currency-type}} is:`);
+  $('.USD-conversion').text(`${USDexchangeReturn} in USD`);
+  $('.CAD-conversion').text(`${CADexchangeReturn} in CAD`);
+  $('.EUR-conversion').text(`${EURexchangeReturn} in EUR`);
+  $('.YEN-conversion').text(`${YENexchangeReturn} in YEN`);
+  $('.HKD-conversion').text(`${HKDexchangeReturn} in HKD`);
+  $('.GBP-conversion').text(`${GBPexchangeReturn} in GBP`);
+  
+}
+
+function displayErrors(error) {
+  $('.show-errors').text(`${error}`);
 }
 
 $(document).ready(function() {
@@ -18,5 +29,23 @@ $(document).ready(function() {
     event.preventDefault();
     let currency = $('#currency').val();
     let currency-type = $('input:radio[name=currency-type]:checked').val();
+    clearFields();
+    Exchange.exchangeMoney(currency)
+      .then(function(exchangeResponse) {
+        if (exchangeResponse instanceof Error) {
+          throw Error(`ExchangeRate-API API error: ${exchangeResponse.message}`);
+        }
+        const USDexchangeReturn = exchangeResponse.USD
+        const CADexchangeReturn = exchangeResponse.CAD;
+        const EURexchangeReturn = exchangeResponse.EUR;
+        const JPYexchangeReturn = exchangeResponse.JPY
+        const HKDexchangeReturn = exchangeResponse.HKD
+        const GBPexchangeReturn = exchangeResponse.GBP
+        
+        displayExchange(CANexchangeReturn);
+      })
+      .catch(function(error) {
+        displayErrors(error.message)
+      })
   });
 });
