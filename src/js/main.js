cd8ed9c5d3998a0e87d3a12d2df0e4c7b913d2a2
Jeff Chiu <jwchiu82@gmail.com>
2020-11-20 10:56:13 -0800

    add radio buttons for picking currency exchange rate

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchange from '../services/exchange-service.js';
+
+function clearFields() {
+  $('#currency').val('');
+  $('.show-errors').text('');
+}
+
+function displayExchange(response) {
+  $('.display-conversion').text(`The exchange rate of ${currency} ${currency-type}}`)
+}
 
 $(document).ready(function() {
   $('#TODO-form').submit(function(event) {
     event.preventDefault();
-
+    let currency = $('#currency').val();
+    let currencyType = $('input:radio[name=currency-type]:checked').val();
   });
 });
