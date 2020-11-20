9ceb85ba957f828d97b63150d1adf160d4e30abe
Jeff Chiu <jwchiu82@gmail.com>
2020-11-20 15:33:20 -0800

    add spacing to results

--- src/js/main.js
+++ src/js/main.js
@@ -6,17 +6,16 @@ import Exchange from '../services/exchange-service.js';
 
 function clearFields() {
   $('.show-errors').text('');
-  //$('#input').hide();
 }
 
 function displayExchange(currency, currencyType, USD, CAD, EUR, YEN, HKD, GBP) {
   $('.display-conversion').text(`The exchange rate of ${currency} ${currencyType} is:`);
-  $('.USD-conversion').text(`${(USD * currency).toFixed(2)} in USD`);
-  $('.CAD-conversion').text(`${(CAD * currency).toFixed(2)} in CAD`);
-  $('.EUR-conversion').text(`${(EUR * currency).toFixed(2)} in EUR`);
-  $('.YEN-conversion').text(`${(YEN * currency).toFixed(2)} in YEN`);
-  $('.HKD-conversion').text(`${(HKD * currency).toFixed(2)} in HKD`);
-  $('.GBP-conversion').text(`${(GBP * currency).toFixed(2)} in GBP`);
+  $('.USD-conv').text(`${(USD * currency).toFixed(2)} in USD`);
+  $('.CAD-conv').text(`${(CAD * currency).toFixed(2)} in CAD`);
+  $('.EUR-conv').text(`${(EUR * currency).toFixed(2)} in EUR`);
+  $('.YEN-conv').text(`${(YEN * currency).toFixed(2)} in YEN`);
+  $('.HKD-conv').text(`${(HKD * currency).toFixed(2)} in HKD`);
+  $('.GBP-conv').text(`${(GBP * currency).toFixed(2)} in GBP`);
 }
 
 function displayErrors(error) {
@@ -26,22 +25,20 @@ function displayErrors(error) {
 $(document).ready(function() {
   $('#money').click(function(event) {
     let currency = parseInt($('#currency').val());
-    //console.log(currency)
     let currencyType = $('input:radio[name=currency-type]:checked').val();
-    //console.log(currencyType)
     clearFields();
     Exchange.exchangeMoney(currencyType)
       .then(function(exchangeResponse) {
         if (exchangeResponse instanceof Error) {
           throw Error(`ExchangeRate-API API error: ${exchangeResponse}`);
         }
-        const USDexchangeReturn = exchangeResponse.conversion_rates.USD;
-        const CADexchangeReturn = exchangeResponse.conversion_rates.CAD;
-        const EURexchangeReturn = exchangeResponse.conversion_rates.EUR;
-        const JPYexchangeReturn = exchangeResponse.conversion_rates.JPY;
-        const HKDexchangeReturn = exchangeResponse.conversion_rates.HKD;
-        const GBPexchangeReturn = exchangeResponse.conversion_rates.GBP;
-        displayExchange(currency, currencyType, USDexchangeReturn, CADexchangeReturn, EURexchangeReturn, JPYexchangeReturn, HKDexchangeReturn, GBPexchangeReturn);
+        const USDex = exchangeResponse.conversion_rates.USD;
+        const CADex = exchangeResponse.conversion_rates.CAD;
+        const EURex = exchangeResponse.conversion_rates.EUR;
+        const JPYex = exchangeResponse.conversion_rates.JPY;
+        const HKDex = exchangeResponse.conversion_rates.HKD;
+        const GBPex = exchangeResponse.conversion_rates.GBP;
+        displayExchange(currency, currencyType, USDex, CADex, EURex, JPYex, HKDex, GBPex);
       })
       .catch(function(error) {
         displayErrors(error.message);
